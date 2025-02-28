27179e6c56e3b0282ae27a9749786d6cf57c4c22
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-28 01:35:18 +0000

    constants_updated

---
+++ circ_circle_tau.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan
+# Created on Feb 27, 2025
+# This module calculates the circumference of a circle by using tau.
+
+# Import the constant from constants.py
