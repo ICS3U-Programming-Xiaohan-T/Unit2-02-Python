e0de87f57549a4da8d825a8b998d971fa17af6ef
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-03-01 00:54:20 +0000

    circ_circle_tau_updated.py

--- circ_circle_tau.py
+++ circ_circle_tau.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan
+# Created on Feb 27, 2025
+# This module calculates the circumference of a circle by using tau.
+# Import the constant from constants.py
+import constants
 
+
+def main():
+    # get the radius from the user's input
+    radius = float(input("Enter the radius of the circle (mm): "))
+    # calculate the circumference by the equation
+    circumference = constants.TAU * radius
+    # print out the result of the circumference
+    print("")
+    print("Circumference = {} mm".format(circumference))
+
+
+if __name__ == "__main__":
+    main()
--- constants.py
+++ constants.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan
+# Created on Feb 27, 2025
+# This module contains constants.
 
+# Define the constant
+TAU = 6.28
